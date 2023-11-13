c2a04dde479bb3f32203b94953f4e7f68c9f95a0
tberkery <78045727+tberkery@users.noreply.github.com>
2023-11-13 12:20:50 -0500

    it's working

--- reference-model/buld_ref_imgs.py
+++ reference-model/buld_ref_imgs.py
@@ -20,14 +20,57 @@ def replace_orange_with_background(image_path):
     # Convert the image from BGR to HSV color space
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+    # Define the lower and upper bounds for the orange color in HSV
+    lower_orange = np.array([10, 100, 100])
+    upper_orange = np.array([25, 255, 255])
+
+    # Create a binary mask for the orange pixels
+    orange_mask = cv2.inRange(hsv_image, lower_orange, upper_orange)
+
+    # Find contours in the orange mask
+    contours, _ = cv2.findContours(orange_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Iterate through each contour (assuming there's only one large orange region)
+    for contour in contours:
+        # Find the bounding box of the orange region
+        x, y, w, h = cv2.boundingRect(contour)
+
+        # Ensure the bounding box coordinates are within the image bounds
+        x, y, w, h = max(0, x), max(0, y), min(w, image.shape[1] - x), min(h, image.shape[0] - y)
+
+        # Extract the region of interest (ROI) from the original image
+        roi = image[y:y + h, x:x + w]
+
+        # Find the background color of the nearest non-orange pixels
+        non_orange_pixels = roi[~orange_mask[y:y + h, x:x + w].astype(bool)]
+        background_color = np.median(non_orange_pixels, axis=0)
+
+        # Fill the ROI with the background color
+        roi[:, :] = np.full_like(roi, background_color)
+
+    return image
+def replace_red_with_background(image_path):
+    # Read the image
+    image = cv2.imread(image_path)
+
+    # Convert the image from BGR to HSV color space
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
     # Define the lower and upper bounds for the red color in HSV
-    lower_red = np.array([10, 100, 100])
-    upper_red = np.array([25, 255, 255])
+    lower_red1 = np.array([0, 100, 100])
+    upper_red1 = np.array([5, 255, 255])
 
-    # Create a binary mask for the red pixels
-    red_mask = cv2.inRange(hsv_image, lower_red, upper_red)
+    lower_red2 = np.array([170, 100, 100])
+    upper_red2 = np.array([180, 255, 255])
 
-    # Find contours in the red mask
+    # Create binary masks for the red pixels
+    red_mask1 = cv2.inRange(hsv_image, lower_red1, upper_red1)
+    red_mask2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
+
+    # Combine the two masks to cover the entire red range
+    red_mask = cv2.bitwise_or(red_mask1, red_mask2)
+
+    # Find contours in the combined red mask
     contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Iterate through each contour (assuming there's only one large red region)
@@ -43,44 +86,6 @@ def replace_orange_with_background(image_path):
 
         # Find the background color of the nearest non-red pixels
         non_red_pixels = roi[~red_mask[y:y + h, x:x + w].astype(bool)]
-        print(non_red_pixels)
-        background_color = np.median(non_red_pixels, axis=0)
-
-        # Fill the ROI with the background color
-        roi[:, :] = np.full_like(roi, background_color)
-
-    return image
-def replace_red_with_background(image_path):
-    # Read the image
-    image = cv2.imread(image_path)
-
-    # Convert the image from BGR to HSV color space
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Define the lower and upper bounds for the red color in HSV
-    lower_red = np.array([0, 100, 100])
-    upper_red = np.array([10, 255, 255])
-
-    # Create a binary mask for the red pixels
-    red_mask = cv2.inRange(hsv_image, lower_red, upper_red)
-
-    # Find contours in the red mask
-    contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Iterate through each contour (assuming there's only one large red region)
-    for contour in contours:
-        # Find the bounding box of the red region
-        x, y, w, h = cv2.boundingRect(contour)
-
-        # Ensure the bounding box coordinates are within the image bounds
-        x, y, w, h = max(0, x), max(0, y), min(w, image.shape[1] - x), min(h, image.shape[0] - y)
-
-        # Extract the region of interest (ROI) from the original image
-        roi = image[y:y+h, x:x+w]
-
-        # Find the background color of the nearest non-red pixels
-        non_red_pixels = roi[~red_mask[y:y+h, x:x+w].astype(bool)]
-        print(non_red_pixels)
         background_color = np.median(non_red_pixels, axis=0)
 
         # Fill the ROI with the background color
@@ -97,7 +102,7 @@ def main():
     orange_result = replace_red_with_background(img_path)
 
     # Display the original image
-    cv2.imshow("Original Image", orig_img)
+    cv2.imshow("Original Image", cv2.resize(orig_img, (500, 500)))
 
     print(orange_result)
     # Display the image with detected orange pixels
